91ada877151b811a270919173d615b118946b6de
NoSky <xfw.nosky@gmail>
2021-02-03 19:43:39 +0800

    update

---
+++ OnlineSurvey_frontEnd/src/components/Header.vue
@@ -0,0 +1,16 @@
+<!-- Description: 网站顶部导航栏 -->
+<template>
+  <div class="header">
+      顶部导航栏
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
+
+<style>
+
+</style>
